1b921bc73cff8e0b30150c2b2fab1ecfbcb687b5
Jia-Wei-Liao <sam23582211@gmail.com>
2024-11-12 22:17:15 +0800

    add convert_mid_to_wav

--- convert_mid_to_wav.py
+++ convert_mid_to_wav.py
@@ -1,13 +1,42 @@
-from midi2audio import FluidSynth
+import os
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+
 import soundfile as sf
+from midi2audio import FluidSynth
 
 
-def midi_to_wav(midi_file, wav_file, soundfont):
+def parse_arguments() -> Namespace:
+    parser = ArgumentParser(description="Symbolic Music Generation")
+
+    # dataset setting
+    parser.add_argument(
+        "--input_folder",
+        type=str,
+        default="results/11-10-10-21-08/task1",
+        help="folder of dataset"
+    )
+    parser.add_argument(
+        "--output_folder",
+        type=str,
+        default="results/11-10-10-21-08/task1_wav",
+        help="folder of dataset"
+    )
+    return parser.parse_args()
+
+
+def midi_to_wav(midi_path, wav_path, soundfont):
     fs = FluidSynth(soundfont)
-    fs.midi_to_audio(midi_file, wav_file)
+    fs.midi_to_audio(midi_path, wav_path)
 
 
-midi_file = 'results/11-10-10-21-08/task2/song_3.mid'
-wav_file = 'output.wav'
-soundfont_file = "Dore Mark's NY S&S Model B-v5.2.sf2"
-midi_to_wav(midi_file, wav_file, soundfont_file)
+if __name__ == '__main__':
+    args = parse_arguments()
+    os.makedirs(args.output_folder, exist_ok=True)
+
+    soundfont_file = "Dore Mark's NY S&S Model B-v5.2.sf2"
+    midi_list = list(Path(args.input_folder).glob("*.mid"))
+
+    for midi_path in midi_list:
+        wav_path = Path(args.output_folder, midi_path.with_suffix(".wav").name)
+        midi_to_wav(midi_path, wav_path, soundfont_file)
